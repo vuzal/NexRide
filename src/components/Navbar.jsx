3ea81b28214d34f77b9d643018949e8b0421e39e
Vusal Abbasov <vusalabbasovredmi9@gmail.com>
2026-05-25 23:03:11 +0400

    Navbar changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,56 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { onAuthStateChanged, signOut } from 'firebase/auth'
+import { auth } from '../firebase'
 import './Navbar.css'
 import logoImg from '../assets/logo.png'
 
 function Navbar() {
-    return (
-        <nav className="navbar">
-            <div className="navbar-logo">
-                <Link to="/">
-                    <img src={logoImg} alt="NexRide Logo" className="logo-image" />
-                    <span className="logo-text">NexRide</span>
-                </Link>
-            </div>
+  const navigate = useNavigate()
+  const [user, setUser] = useState(null)
 
-            <div className="navbar-links">
-                <NavLink to="/" end>Home</NavLink>
-                <NavLink to="/cars">Cars</NavLink>
-                <NavLink to="/list-car" className="list-car-btn">List Your Car</NavLink>
-            </div>
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser)
+    })
+    return () => unsubscribe()
+  }, [])
 
-            <div className="navbar-auth">
-                <Link to="/login" className="login-link">Sign In</Link>
-            </div>
-        </nav>
-    )
+  async function handleSignOut() {
+    await signOut(auth)
+    navigate('/')
+  }
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">
+          <img src={logoImg} alt="NexRide Logo" className="logo-image" />
+          <span className="logo-text">NexRide</span>
+        </Link>
+      </div>
+
+      <div className="navbar-links">
+        <NavLink to="/" end>Home</NavLink>
+        <NavLink to="/cars">Cars</NavLink>
+        <NavLink to="/list-car" className="list-car-btn">List Your Car</NavLink>
+      </div>
+
+      <div className="navbar-auth">
+        {user ? (
+          <>
+            <span className="navbar-username">{user.displayName || user.email}</span>
+            <button className="signout-btn" onClick={handleSignOut}>Sign Out</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login" className="login-link">Sign In</Link>
+            <Link to="/signup" className="signup-btn">Sign Up</Link>
+          </>
+        )}
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
